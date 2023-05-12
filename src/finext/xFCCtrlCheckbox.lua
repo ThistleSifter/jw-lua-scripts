cf2839f86f1ea05e92680c052e1a05697e613393
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2023-05-12 18:13:20 +0930

    Update xFCCtrlCheckbox.lua

--- src/finext/xFCCtrlCheckbox.lua
+++ src/finext/xFCCtrlCheckbox.lua
@@ -5,12 +5,14 @@ $module xFCCtrlCheckbox
 
 ## Summary of Modifications
 - Added `CheckChange` custom control event.
+- Added hooks into control state preservation.
 ]] --
 local finext = require("library.finext")
 local utils = require("library.utils")
 
 local class = {Methods = {}}
 local methods = class.Methods
+local private = setmetatable({}, {__mode = "k"})
 
 local check_change = finext.xFCCustomLuaWindow.CreateCustomControlChangeEvent(
     -- initial could be set to -1 to force the event to fire on InitWindow, but unlike other controls, -1 is not a valid checkstate.
@@ -23,20 +25,58 @@ local check_change = finext.xFCCustomLuaWindow.CreateCustomControlChangeEvent(
 )
 
 --[[
+% Init
+
+**[Internal]**
+
+@ self (xFCCtrlCheckbox)
+]]
+function class:Init()
+    if private[self] then
+        return
+    end
+
+    private[self] = {}
+end
+
+--[[
 % SetCheck
 
 **[Fluid] [Override]**
 
 Override Changes:
 - Ensures that `CheckChange` event is triggered.
+- Hooks into control state preservation.
 
 @ self (xFCCtrlCheckbox)
 @ checked (number)
 ]]
 function methods:SetCheck(checked)
-    utils.call_and_rethrow(2, self.__.SetCheck, self.__, checked)
+    finext_helper.assert_argument_type(2, checked, "number")
+
+    if finext.xFCControl.UseStoredControlState(self) then
+        private[self].Check = checked
+    else
+        self.__:SetCheck(checked)
+    end
 
     check_change.trigger(self)
+end
+
+--[[
+% GetCheck
+
+Override Changes:
+- Hooks into control state preservation.
+
+@ self (xFCCtrlCheckbox)
+]]
+function methods:GetCheck()
+    if finext.xFCControl.UseStoredControlState(self) then
+        return private[self].Check
+    end
+
+    return self.__:GetCheck()
 end
 
 --[[
@@ -45,7 +85,7 @@ end
 **[Callback Template]**
 
 @ control (xFCCtrlCheckbox) The control that was changed.
-@ last_check (string) The previous value of the control's check state..
+@ last_check (number) The previous value of the control's check state..
 ]]
 
 --[[
